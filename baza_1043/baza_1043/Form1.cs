2747697e5bd32951394d457b975df0d5d5b75ebc
stefan <lazeastefan@gmail.com>
2019-05-17 11:07:02 +0300

    add project and other labs

---
+++ baza_1043/baza_1043/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; //pentru Access
+
+namespace baza_1043
+{
+    public partial class Form1 : Form
+    {
+        string connString;
+        public Form1()
+        {
+            InitializeComponent();
+            connString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = comenzi.accdb";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear(); // o sa stearga doar inregistrarile; pentru a nu fi dublate
+            OleDbConnection conexiune = new OleDbConnection(connString);
+            OleDbCommand comanda = new OleDbCommand("SELECT * FROM comenzi", conexiune);
+
+
+            try
+            {
+                conexiune.Open();
+                OleDbDataReader reader = comanda.ExecuteReader(); //un obiect de tip datareader; container
+                while (reader.Read())
+                {
+                    //ptr a popula list item ul
+
+                    ListViewItem itm = new ListViewItem(reader["cod"].ToString());
+                    itm.SubItems.Add(reader["denumire"].ToString());
+                    itm.SubItems.Add(reader["nrProd"].ToString());
+                    itm.SubItems.Add(reader["tipPlata"].ToString());
+                    itm.SubItems.Add(reader["valoare"].ToString());
+                    listView1.Items.Add(itm);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexiune.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conexiune = new OleDbConnection(connString);
+            OleDbCommand comanda = new OleDbCommand();
+            comanda.Connection = conexiune; //al doilea parametru
+
+            try
+            {
+                conexiune.Open();
+                foreach (ListViewItem itm in listView1.Items)
+                {
+                    if (itm.Checked)
+                    {
+                        int cod = Convert.ToInt32(itm.SubItems[0].Text);
+                        comanda.CommandText = "DELETE FROM comenzi where cod=" + cod;// "'";
+                        comanda.ExecuteNonQuery(); //comenzile pentru manipulare
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexiune.Close();
+            }
+            button1_Click(sender, e);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+    }
+}
---
+++ baza_1043/baza_1043/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace baza_1043
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = comenzi.accdb";
+            OleDbConnection conexiune = new OleDbConnection(connString);
+            OleDbCommand comanda = new OleDbCommand();
+            comanda.Connection = conexiune; //al doilea parametru
+
+            try
+            {
+                conexiune.Open();
+                comanda.CommandText = "Select MAX(cod) FROM comenzi";
+                int cod = Convert.ToInt32(comanda.ExecuteScalar()); // returneaza valoarea
+                comanda.CommandText = "INSERT INTO comenzi VALUES(?,?,?,?,?)";
+                comanda.Parameters.Add("cod", OleDbType.Integer).Value = cod + 1;
+                //comanda.Parameters["cod"].Value = cod + 1;
+                comanda.Parameters.Add("denumire", OleDbType.Char,30).Value = tbDenumire.Text;
+                comanda.Parameters.Add("nrProd", OleDbType.Integer).Value = Convert.ToInt32(tbNrProduse.Text);
+                comanda.Parameters.Add("tipPlata", OleDbType.Char, 20).Value = cbPlata.Text;
+                comanda.Parameters.Add("valoare", OleDbType.Integer).Value = Convert.ToInt32(tbValoare.Text);
+                comanda.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexiune.Close();
+                tbDenumire.Clear();
+                tbNrProduse.Clear();
+                tbValoare.Clear();
+                cbPlata.Text = "";
+            }
+        }
+    }
+}
