d827fb994067b907a324f3febcfcbdfd0fd30367
stefan <lazeastefan@gmail.com>
2019-05-20 18:15:35 +0300

    Auto stash before rebase of "origin/master"

---
+++ Proiect/1043_Lazea_Stefan/1043_Lazea_Stefan/Categorii.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1043_Lazea_Stefan
+{
+    class Categorii
+    {
+        private int id;
+        private string denumire;
+        
+        public Categorii(int id, string denumire)
+        {
+            this.id = id;
+            this.denumire = denumire;
+        }
+
+        public override string ToString()
+        {
+            return this.denumire;
+        }
+    }
+}
